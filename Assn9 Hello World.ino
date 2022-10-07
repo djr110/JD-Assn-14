c42e2f1f3b659debcd3d898254636d0304055483
djr110 <115322426+djr110@users.noreply.github.com>
2022-10-07 17:44:53 -0400

    Add files via upload

---
+++ Assn9 Hello World.ino
@@ -0,0 +1,18 @@
+int value = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  pinMode(9,INPUT);
+
+}
+
+void loop() {
+  value = digitalRead(9);
+  
+  digitalWrite(8,value);
+  delay(1000);
+  digitalWrite(8,LOW);
+  delay(1000);
+  
+}
